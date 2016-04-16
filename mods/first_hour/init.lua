d5aa43679aa789794ec34bc77c57d91ecff112d0
paly2 <plangrognet@laposte.net>
2016-04-16 15:49:14 +0200

    Updating first_hour mod

--- mods/first_hour/init.lua
+++ mods/first_hour/init.lua
@@ -2,14 +2,15 @@ local players = {}
 
 minetest.register_on_newplayer(function(player)
 	local name = player:get_player_name()
-	minetest.chat_send_player(name, "Bienvenue sur le serveur Mynetest ! Vous êtes invicible et ne pouvez pas frapper les autres joueurs pendant une heure.\nWelcome to the Mynetest server ! You are invicible and you can't hit players during an hour.")
-	formspec = "size[15,3;]"..
-	           "label[.50,.50;Bienvenue sur le serveur Mynetest ! Vous êtes invicible et ne pouvez pas frapper les autres joueurs pendant une heure.\nWelcome to the Mynetest server ! You are invicible and you can't hit players during an hour.]"
+	minetest.chat_send_player(name, "Bienvenue sur le serveur Mynetest ! Vous êtes invicible et ne pouvez pas frapper les autres joueurs pendant une heure.\nWelcome to the Mynetest server ! You are invicible and you can't hit players during one hour.")
+	formspec = "size[10,3;]"..
+	           "label[.50,.50;Bienvenue sur le serveur Mynetest ! Vous êtes invicible et ne pouvez pas frapper les autres joueurs pendant une heure.\nWelcome to the Mynetest server ! You are invicible and you can't hit players during one hour.]"..
+	           "button_exit[4,2.5;2,1;exit;Ok]"
 	minetest.show_formspec(name, "first_hour:welcome", formspec)
 	
 	
 	table.insert(players, name)
-	minetest.after(60, function(name, pos)
+	minetest.after(3600, function(name, pos)
 		minetest.chat_send_player(name, "It's not your first hour anymore... You're not invicible and you can hit other players now. Ce n'est plus votre première heure... Vous n'êtes plus invicible et vous pouvez frapper les autres joueurs maintenant.")
 		table.remove(players, pos)
 	end, name, table.getn(players))
