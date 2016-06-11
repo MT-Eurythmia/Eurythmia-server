49f3d1c77d51fcf506d5fae4557b98546c655854
paly2 <plangrognet@laposte.net>
2016-06-11 11:29:51 +0200

    Change run.sh : don't restart if the server returned 0

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ### Running MTSatellite
-SATPATH="$PWD/../mtsatellite"
+SATPATH="/home/administrateur/.minetest/mtsatellite"
 export GOROOT=$SATPATH/go
 export GOPATH=$SATPATH/gopath
 
-LOGFILE="$PWD/../mtsatellite_log.txt"
+LOGFILE="/home/administrateur/.minetest/mtsatellite_log.txt"
 
-$GOPATH/bin/mtredisalize -host=localhost -interleaved=true -change-url=http://localhost:8808/update -change-duration=60s $PWD/../worlds/server/map.db >> $LOGFILE 2>&1 &
+$GOPATH/bin/mtredisalize -host=localhost -interleaved=true -change-url=http://localhost:8808/update -change-duration=60s /home/administrateur/.minetest/worlds/server/map.db >> $LOGFILE 2>&1 &
 
 sleep 1
 
-GOMAXPROCS=2 $GOPATH/bin/mtwebmapper -colors=$SATPATH/colors.txt -web-host="" -map=$SATPATH/map_satellite -web=$SATPATH/web -redis-host=localhost -workers=1 -websockets=false -players=/tmp/mt_players_fifo >> $LOGFILE 2>&1 &
+cpulimit -c 1 -l 1 -b -- $GOPATH/bin/mtwebmapper -colors=$SATPATH/colors.txt -web-host="" -map=$SATPATH/map_satellite -web=$SATPATH/web -redis-host=localhost -workers=1 -websockets=false -players=/tmp/mt_players_fifo -ymax=500 >> $LOGFILE 2>&1
 
 sleep 1
 
@@ -20,14 +20,13 @@ while [ true ]; do
 	
 	/home/administrateur/minetest/bin/minetestserver
 	if [ $? = 0 ]; then # Normal quit : Maybe the admin pressed Ctrl+C. Wait before restarting.
-		echo "Please press Ctrl+C (again). If you don't, the server will restart in 5 seconds."
-		sleep 5
+		exit
 	else
 		echo "Crash! Restarting the server immediatly..."
-		sleep 0.1 # Sleep a little to avoid a total crashing loop
+		sleep 1 # Sleep a second to avoid a total crashing loop
 	fi
 	
 	# Run again mtredisalize (it stops automatically)
-	$GOPATH/bin/mtredisalize -host=localhost -interleaved=true -change-url=http://localhost:8808/update -change-duration=60s $PWD/../worlds/server/map.db >> $LOGFILE 2>&1 &
+	$GOPATH/bin/mtredisalize -host=localhost -interleaved=true -change-url=http://localhost:8808/update -change-duration=60s /home/administrateur/.minetest/worlds/server/map.db >> $LOGFILE 2>&1 &
 	sleep 1
 done
