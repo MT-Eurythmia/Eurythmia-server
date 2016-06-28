e9b95f559d1c4a57084c2cac70e0713fda04d8a3
paly2 <plangrognet@laposte.net>
2016-06-28 16:14:08 +0200

    Use self.player instead of obj as puncher for arrow and golden arrow Fix #8

--- mods/throwing/golden_arrow.lua
+++ mods/throwing/golden_arrow.lua
@@ -45,12 +45,17 @@ THROWING_arrow_gold_ENTITY.on_step = function(self, dtime)
 	local node = minetest.get_node(pos)
 
 	if self.timer > 0.2 then
+		local hitter = self.player
+		if not hitter then
+			return
+		end
+		
 		local objs = minetest.get_objects_inside_radius({x = pos.x, y = pos.y, z = pos.z}, 1)
 		for k, obj in pairs(objs) do
 			if obj:get_luaentity() ~= nil then
 				if obj:get_luaentity().name ~= "throwing:arrow_gold_entity" and obj:get_luaentity().name ~= "__builtin:item" then
 					local damage = 5
-					obj:punch(self.object, 1.0, {
+					obj:punch(hitter, 1.0, {
 						full_punch_interval = 1.0,
 						damage_groups= {fleshy = damage},
 					}, nil)
@@ -59,7 +64,7 @@ THROWING_arrow_gold_ENTITY.on_step = function(self, dtime)
 				end
 			else
 				local damage = 5
-				obj:punch(self.object, 1.0, {
+				obj:punch(hitter, 1.0, {
 					full_punch_interval = 1.0,
 					damage_groups= {fleshy = damage},
 				}, nil)
