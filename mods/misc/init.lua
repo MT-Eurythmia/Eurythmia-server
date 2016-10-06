e7cbe9fd4be4362b3cadda112bf42f9e45e37ae4
paly2 <plangrognet@laposte.net>
2016-10-06 17:53:29 +0200

    Boost_cart: allow power rails to work without mesecons

--- mods/misc/init.lua
+++ mods/misc/init.lua
@@ -274,3 +274,24 @@ minetest.register_lbm({
 		minetest.set_node(pos, {name = "air"})
 	end,
 })
+
+--[[
+Boost cart: allow power rails to work without mesecons.
+]]
+minetest.override_item(":carts:powerrail", {
+	after_place_node = function(pos, placer, itemstack)
+		minetest.get_meta(pos):set_string("cart_acceleration", "0.5")
+	end,
+
+	mesecons = {
+		effector = {
+			action_on = function(pos, node)
+				return
+			end,
+
+			action_off = function(pos, node)
+				return
+			end
+		}
+	}
+})
