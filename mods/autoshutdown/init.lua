f39fd8a7072ea4c3fa55c8d2e5c7f6708cf6fada
minetest-admins <plangrognet@laposte.net>
2016-11-19 16:49:23 +0100

    Add the autoshutdown mod

---
+++ mods/autoshutdown/init.lua
@@ -0,0 +1,24 @@
+-- The backup is scheduled at 04:30, so request shutdown at 04:27
+local shutdown_time = os.time{year=2000, month=1, day=1, hour=4, minute=27} -- don't care of the year, month and day
+
+local CHECK_INTERVAL = 1000
+local check_timer = 0
+minetest.register_globalstep(function(dtime)
+	check_timer = check_timer + dtime
+	if check_timer < CHECK_INTERVAL then
+		return
+	end
+	check_timer = 0
+
+	if os.date("%H") == os.date("%H", shutdown_time) or
+	    (tonumber(os.date("%M", shutdown_time)) < 10 and
+	    os.date("%H") == os.date("%H", shutdown_time - 10 * 60)) then
+		if os.date("%M") == os.date("%M", shutdown_time - 10 * 60) then
+			minetest.chat_send_all("Info: the server will shutdown for backup in 10 minutes.")
+		elseif os.date("%M") == os.date("%M", shutdown_time - 5 * 60) then
+			minetest.chat_send_all("Info: the server will shutdown for backup in 5 minutes.")
+		elseif os.date("%M") == os.date("%M", shutdown_time) then
+			minetest.request_shutdown("*** Shutting down for backup now ! ***", false)
+		end
+	end
+end)
