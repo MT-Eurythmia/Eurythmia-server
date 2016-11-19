c7ef6069f3831fdb6fc4705cde114b28540f1f4d
minetest-admins <plangrognet@laposte.net>
2016-11-19 16:51:33 +0100

    Add a backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Stop the server if is not
+/etc/init.d/minetestserver stop
+
+if [ `date +%d` = '1' ]; then
+	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_monthly
+elif [ `date +%w` = '1' ]; then
+	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_weekly
+else
+	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_nightly
+fi
+
+# Restart the server
+/etc/init.d/minetestserver start
