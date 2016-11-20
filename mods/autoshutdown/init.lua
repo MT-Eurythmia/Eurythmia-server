deedfcfec725b5a94f1e657d40174c027f966faa
minetest-admins <plangrognet@laposte.net>
2016-11-20 11:06:01 +0100

    Autoshutdown: fix check interval time, add a message a minute before shutdown

--- mods/autoshutdown/init.lua
+++ mods/autoshutdown/init.lua
@@ -1,7 +1,7 @@
 -- The backup is scheduled at 04:30, so request shutdown at 04:27
 local shutdown_time = os.time{year=2000, month=1, day=1, hour=4, minute=27} -- don't care of the year, month and day
 
-local CHECK_INTERVAL = 1000
+local CHECK_INTERVAL = 60
 local check_timer = 0
 minetest.register_globalstep(function(dtime)
 	check_timer = check_timer + dtime
@@ -17,6 +17,8 @@ minetest.register_globalstep(function(dtime)
 			minetest.chat_send_all("Info: the server will shutdown for backup in 10 minutes.")
 		elseif os.date("%M") == os.date("%M", shutdown_time - 5 * 60) then
 			minetest.chat_send_all("Info: the server will shutdown for backup in 5 minutes.")
+		elseif os.date("%M") == os.date("%M", shutdown_time - 60) then
+                        minetest.chat_send_all("Info: the server will shutdown for backup in a minute.")
 		elseif os.date("%M") == os.date("%M", shutdown_time) then
 			minetest.request_shutdown("*** Shutting down for backup now ! ***", false)
 		end
