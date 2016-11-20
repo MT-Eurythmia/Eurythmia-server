877b954b5bad6c485ba8c2b5f72f86c273f646a4
minetest-admins <plangrognet@laposte.net>
2016-11-20 11:15:12 +0100

    Backup script: add slashes to src paths to avoid creating an additional directory level, use --archive option And remove --recursive option, inplicitely specified in --archive

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,11 +4,11 @@
 /etc/init.d/minetestserver stop
 
 if [ `date +%d` = '1' ]; then
-	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_monthly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_monthly
 elif [ `date +%w` = '1' ]; then
-	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_weekly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_weekly
 else
-	rsync --delete --recursive /home/minetest/.minetest/worlds/server /home/minetest/.minetest/worlds/server_nightly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_nightly
 fi
 
 # Restart the server
