13aecead56cf21e72b0fe0c269886af7e68e6337
minetest-admins <plangrognet@laposte.net>
2016-11-24 18:46:25 +0100

    Autoshutdown: fix server shutting down at 03:00

--- mods/autoshutdown/init.lua
+++ mods/autoshutdown/init.lua
@@ -1,5 +1,5 @@
 -- The backup is scheduled at 04:30, so request shutdown at 04:27
-local shutdown_time = {year=2000, month=1, day=1, hour=4, minute=27} -- don't care of the year, month and day
+local shutdown_time = {year=2000, month=1, day=1, hour=4, min=27} -- don't care of the year, month and day
 
 local CHECK_INTERVAL = 60
 local check_timer = 0
@@ -10,17 +10,13 @@ minetest.register_globalstep(function(dtime)
 	end
 	check_timer = 0
 
-	if os.date("%H") == os.date("%H", os.time(shutdown_time)) or
-	    (tonumber(os.date("%M", os.time(shutdown_time))) < 10 and
-	    os.date("%H") == os.date("%H", os.time(shutdown_time) - 10 * 60)) then
-		if os.date("%M") == os.date("%M", os.time(shutdown_time) - 10 * 60) then
-			minetest.chat_send_all("Info: the server will shutdown for backup in 10 minutes.")
-		elseif os.date("%M") == os.date("%M", os.time(shutdown_time) - 5 * 60) then
-			minetest.chat_send_all("Info: the server will shutdown for backup in 5 minutes.")
-		elseif os.date("%M") == os.date("%M", os.time(shutdown_time) - 60) then
-                        minetest.chat_send_all("Info: the server will shutdown for backup in a minute.")
-		elseif os.date("%M") == os.date("%M", os.time(shutdown_time)) then
-			minetest.request_shutdown("*** Shutting down for backup now ! ***", false)
-		end
+	if os.date("%H:%M") == os.date("%H:%M", os.time(shutdown_time) - 10 * 60) then
+		minetest.chat_send_all("Info: the server will shutdown for backup in 10 minutes.")
+	elseif os.date("%H:%M") == os.date("%H:%M", os.time(shutdown_time) - 5 * 60) then
+		minetest.chat_send_all("Info: the server will shutdown for backup in 5 minutes.")
+	elseif os.date("%H:%M") == os.date("%H:%M", os.time(shutdown_time) - 60) then
+		minetest.chat_send_all("Info: the server will shutdown for backup in a minute.")
+	elseif os.date("%H:%M") == os.date("%H:%M", os.time(shutdown_time)) then
+		minetest.request_shutdown("*** Shutting down for backup now ! ***", false)
 	end
 end)
