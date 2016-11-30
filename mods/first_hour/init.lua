fc8a0adb6852e7bb87df571ad660eaeffb819c49
AcidNinjaFWHR <fabienwang@free.fr>
2016-11-30 02:23:53 +0100

    [WIP] no interract unless a player played 5 minutes + he entered the code in the chat.

--- mods/first_hour/init.lua
+++ mods/first_hour/init.lua
@@ -1,4 +1,5 @@
 local players = {}
+local played_300s = {}
 
 local function begin_game_english(name)
 	for _, player in ipairs(players) do
@@ -20,22 +21,33 @@ local function begin_game_english(name)
 	minetest.show_formspec(name, "first_hour:welcome", formspec)
 
 	minetest.after(300, function(name) -- After five minutes
-		local privs = minetest.get_player_privs(name)
-		privs.interact = true
-		minetest.set_player_privs(name, privs)
-		minetest.log("info", "Player "..name.." got the interact privilege.")
-
-		local player = minetest.get_player_by_name(name)
-		if not player then -- If the player has disconnected
-			return
-		end
-
-		player:setpos({x = 0, y = 18, z = 0})
-
-		minetest.chat_send_player(name, "Let's play, you got the interact privilege! If you didn't read the signs, you can still do it using /spawn :-)")
-
-		minetest.chat_send_all(name..", nouveau joueur anglophone, a rejoint le jeu!\nThe new English-speaker player "..name.." joined the game!")
+		played_300s[name] = true
 	end, name)
+
+	minetest.register_on_chat_message(function(name, message)
+		if message == "341C" then
+			if played_300s[name] ~ nil then
+				local privs = minetest.get_player_privs(name)
+				privs.interact = true
+				minetest.set_player_privs(name, privs)
+				minetest.log("info", "Player "..name.." got the interact privilege.")
+
+				local player = minetest.get_player_by_name(name)
+				if not player then -- If the player has disconnected
+					return
+				end
+
+				played_300s[name] = nil
+				player:setpos({x = 0, y = 18, z = 0})
+
+				minetest.chat_send_player(name, "Let's play, you got the interact privilege! If you didn't read the signs, you can still do it using /spawn")
+				minetest.chat_send_all(name..", nouveau joueur anglophone, a rejoint le jeu!\nThe new English-speaker player "..name.." joined the game!")
+			else
+				minetest.chat_send_player(name, "You haven't been here 5 minutes yet. Wait a few moment and then retry it! If you didn't read all the signs, please do it")
+			end
+
+		end
+	end)
 
 	minetest.after(3600, function(name, pos) -- After a hour
 		minetest.chat_send_player(name, "It is not your first hour anymore... You are now not invincible and you are able to hit other players.")
@@ -44,6 +56,7 @@ local function begin_game_english(name)
 
 		minetest.log("info", "End of first hour of player "..name)
 	end, name, table.getn(players))
+
 end
 
 local function begin_game_french(name)
@@ -66,23 +79,33 @@ local function begin_game_french(name)
 	minetest.show_formspec(name, "first_hour:welcome", formspec)
 
 	minetest.after(300, function(name) -- After five minutes
-		local privs = minetest.get_player_privs(name)
-		privs.interact = true
-		minetest.set_player_privs(name, privs)
-		minetest.log("info", "Player "..name.." got the interact privilege.")
-
-		local player = minetest.get_player_by_name(name)
-		if not player then -- If the player has disconnected
-			return
-		end
-
-
-		player:setpos({x = 0, y = 18, z = 0})
-
-		minetest.chat_send_player(name, "Vous pouvez maintenant commencer à jouer, vous avez obtenu le privilège interact! Si vous n'avez pas eu le temps de lire les panneaux, vous pouvez le faire au moyen de /spawn :-)")
-
-		minetest.chat_send_all(name..", nouveau joueur français, a rejoint le jeu!\nThe new French player "..name.." joined the game!")
+		played_300s[name] = true
 	end, name)
+
+	minetest.register_on_chat_message(function(name, message)
+		if message == "341C" then
+			if played_300s[name] ~ nil then
+				local privs = minetest.get_player_privs(name)
+				privs.interact = true
+				minetest.set_player_privs(name, privs)
+				minetest.log("info", "Player "..name.." got the interact privilege.")
+
+				local player = minetest.get_player_by_name(name)
+				if not player then -- If the player has disconnected
+					return
+				end
+
+				played_300s[name] = nil
+				player:setpos({x = 0, y = 18, z = 0})
+
+				inetest.chat_send_player(name, "Vous pouvez maintenant commencer à jouer, vous avez obtenu le privilège interact! Si vous n'avez pas eu le temps de lire les panneaux, vous pouvez le faire au moyen de /spawn :-)")
+				minetest.chat_send_all(name..", nouveau joueur français, a rejoint le jeu!\nThe new French player "..name.." joined the game!")
+			else
+				minetest.chat_send_player(name, "Vous n'avez pas encore joué 5 minutes ici. Patientez un peu et réessayez! Si vous n'avez pas lu tous les panneaux, veuillez le faire merci.")
+			end
+
+		end
+	end)
 
 	minetest.after(3600, function(name, pos) -- After a hour
 		minetest.chat_send_player(name, "Ce n'est plus votre première heure... Vous n'êtes plus invicible et vous pouvez frapper les autres joueurs à présent.")
@@ -100,7 +123,7 @@ minetest.register_on_newplayer(function(player)
 	                 "label[0,0;Merci de choisir votre langue.\nPlease choose your preferred language.]"..
 	                 "button[0,1;2,1;fr;Français]button[2,1;2,1;en;English]"
 	minetest.show_formspec(name, "first_hour:language_select", formspec)
-	
+
 	minetest.log("info", "First hour of player "..name.." begins")
 end)
 
@@ -110,7 +133,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	end
 
 	local name = player:get_player_name()
-	
+
 	if fields["en"] then
 		begin_game_english(name)
 	elseif fields["fr"] then
