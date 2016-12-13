e63496ecb173c5a9b32f12e22521fed1cc31a154
AcidNinja <fabienwang@free.fr>
2016-12-13 17:14:56 +0100

    Let logrotate decide what to do
    Every night, instead of launching it 1 time a week.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,10 +7,12 @@ if [ `date +%d` = '1' ]; then
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_monthly
 elif [ `date +%w` = '1' ]; then
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_weekly
-	logrotate /home/minetest/scripts/logrotate_conf
 else
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_nightly
 fi
+
+# Logrotate
+logrotate /home/minetest/scripts/logrotate_conf
 
 # Restart the server
 /etc/init.d/minetestserver start
