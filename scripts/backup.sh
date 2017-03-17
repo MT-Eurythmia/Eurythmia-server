41753c612e24e3ae9eea01da9b646f4927848595
minetest-admins <plangrognet@laposte.net>
2017-03-17 14:34:35 +0100

    [scripts/backup.sh] Back up the world directory to /backupd/minetest directory (on the backup drive)

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,12 +3,12 @@
 # Stop the server if is not
 /etc/init.d/minetestserver stop
 
-if [ `date +%d` = '1' ]; then
-	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_monthly
+if [ `date +%d` = '01' ]; then
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_monthly
 elif [ `date +%w` = '1' ]; then
-	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_weekly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_weekly
 else
-	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /home/minetest/.minetest/worlds/server_nightly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_nightly
 fi
 
 # Logrotate
