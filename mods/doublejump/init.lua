0b1b86d6863a9871d3c368ec8e0d2a14b886ff90
upsilon <upsilon@langg.net>
2017-04-03 19:25:14 +0200

    [doublejump] Some new fixes...

--- mods/doublejump/init.lua
+++ mods/doublejump/init.lua
@@ -2,19 +2,22 @@ local sneaking_players = {}
 minetest.register_globalstep(function(dtime)
 	local players = minetest.get_connected_players()
 	for _, player in ipairs(players) do
-		if player:get_player_control().sneak and not sneaking_players[player] then
-			local physics = player:get_physics_override()
-			if physics.jump == 1 then -- Check if it's the default value. Don't change it otherwise.
-				sneaking_players[player] = true
-				physics.jump = 1.4
-				player:set_physics_override(physics)
+		local name = player:get_player_name()
+		if player:get_player_control().sneak and not sneaking_players[name] then
+			print(player:get_physics_override().jump, name)
+			if player:get_physics_override().jump == 1.0 then -- Check if it's the default value. Don't change it otherwise.
+				print(player:get_physics_override().jump, name)
+				sneaking_players[name] = true
+				player:set_physics_override({jump = 1.4})
 			end
-		elseif not player:get_player_control().sneak and sneaking_players[player] then
-			sneaking_players[player] = nil
-			local physics = player:get_physics_override()
-			if physics.jump == 1.4 then -- If some other mod didn't change the value while the player was sneaking
-				physics.jump = 1
-				player:set_physics_override(physics)
+		elseif not player:get_player_control().sneak and sneaking_players[name] then
+			print("not", player:get_physics_override().jump, name)
+			sneaking_players[name] = nil
+			 -- If some other mod didn't change the value while the player was sneaking
+			 -- There's a bug that makes that this value is not exactly 1.4 (it is more close to 1.3999999761581)
+			if player:get_physics_override().jump > 1.3 and player:get_physics_override().jump < 1.5 then
+				print("not", player:get_physics_override().jump, name)
+				player:set_physics_override({jump = 1.0})
 			end
 		end
 	end
