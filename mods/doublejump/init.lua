57b7c433f26a8f32e0306a9dff8e86df0c44810f
upsilon <upsilon@langg.net>
2017-04-05 20:10:18 +0200

    [doublejump] Remove debug prints

--- mods/doublejump/init.lua
+++ mods/doublejump/init.lua
@@ -4,19 +4,15 @@ minetest.register_globalstep(function(dtime)
 	for _, player in ipairs(players) do
 		local name = player:get_player_name()
 		if player:get_player_control().sneak and not sneaking_players[name] then
-			print(player:get_physics_override().jump, name)
 			if player:get_physics_override().jump == 1.0 then -- Check if it's the default value. Don't change it otherwise.
-				print(player:get_physics_override().jump, name)
 				sneaking_players[name] = true
 				player:set_physics_override({jump = 1.4})
 			end
 		elseif not player:get_player_control().sneak and sneaking_players[name] then
-			print("not", player:get_physics_override().jump, name)
 			sneaking_players[name] = nil
 			 -- If some other mod didn't change the value while the player was sneaking
 			 -- There's a bug that makes that this value is not exactly 1.4 (it is more close to 1.3999999761581)
 			if player:get_physics_override().jump > 1.3 and player:get_physics_override().jump < 1.5 then
-				print("not", player:get_physics_override().jump, name)
 				player:set_physics_override({jump = 1.0})
 			end
 		end
