073c4b55ef624a5e9aaca603d56ebf4fd45aa93d
upsilon <upsilon@langg.net>
2017-06-12 19:27:15 +0200

    Add auto-update script The server files are automatically updated from the repo every nights.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,7 @@
 # Stop the server if is not
 /etc/init.d/minetestserver stop
 
+# Map backup
 if [ `date +%d` = '01' ]; then
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_monthly
 elif [ `date +%w` = '1' ]; then
@@ -13,6 +14,9 @@ fi
 
 # Logrotate
 /usr/sbin/logrotate /home/minetest/scripts/logrotate_conf
+
+# Update files from the git repo
+/home/minetest/scripts/update.sh
 
 # Restart the server
 /etc/init.d/minetestserver start
---
+++ scripts/update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /home/minetest/eurythmia-server && git fetch --quiet origin master && git reset --quiet --hard FETCH_HEAD && git clean -df && git submodule update --init
+rsync --delete -a /home/minetest/eurythmia-server/mods/ /home/minetest/.minetest/mods/
+rsync --delete -a /home/minetest/eurythmia-server/games/ /home/minetest/.minetest/games/
+rsync -lptgo /home/minetest/eurythmia-server/minetest.conf /home/minetest/.minetest/
+rsync -lptgo /home/minetest/eurythmia-server/worlds/server/world.mt /home/minetest/.minetest/worlds/server/
