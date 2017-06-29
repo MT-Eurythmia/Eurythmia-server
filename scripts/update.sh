8f50c38099013c09a4d575276620f09923fd7b8b
upsilon <upsilon@langg.net>
2017-06-29 20:32:30 +0200

    Update.sh script: sync submodules URLs

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/minetest/eurythmia-server && git fetch --quiet origin master && git reset --quiet --hard FETCH_HEAD && git clean -df && git submodule update --init --quiet
+cd /home/minetest/eurythmia-server && git fetch --quiet origin master && git reset --quiet --hard FETCH_HEAD && git clean -df && git submodule sync --quiet && git submodule update --init --quiet
 rsync --delete -a /home/minetest/eurythmia-server/mods/ /home/minetest/.minetest/mods/
 rsync --delete -a /home/minetest/eurythmia-server/games/ /home/minetest/.minetest/games/
 rsync -lptgo /home/minetest/eurythmia-server/minetest.conf /home/minetest/.minetest/
