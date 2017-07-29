73b47e309dff8993cc6901850343ae1ace7a3cf0
upsilon <upsilon@langg.net>
2017-07-29 17:49:33 +0200

    [backup.sh] Conserve the nightly backups of the 3 last days

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo -e '=> **** Running script backup.sh ****\n'
 
@@ -8,20 +8,26 @@ killall minetestserver
 
 # Map backup
 echo '==> Backuping the map.'
+echo '===> Rotating nightly backups'
+rm /backupd/minetest/server_nightly_j-2
+mv /backupd/minetest/server_nightly_j-1 /backupd/minetest/server_nightly_j-2
+mv /backupd/minetest/server_nightly_j-0 /backupd/minetest/server_nightly_j-1
 if [ `date +%d` = '01' ]; then
 	echo '===> Backuping to /backupd/minetest/server_monthly...'
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_monthly
+	ln -s /backupd/minetest/server_monthly /backupd/minetest/server_nightly_j-0
 elif [ `date +%w` = '1' ]; then
 	echo '===> Backuping to /backupd/minetest/server_weekly...'
 	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_weekly
+	ln -s /backupd/minetest/server_weekly /backupd/minetest/server_nightly_j-0
 else
 	echo '===> Backuping to /backupd/minetest/server_nightly...'
-	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_nightly
+	rsync --delete --archive /home/minetest/.minetest/worlds/server/ /backupd/minetest/server_nightly_j-0
 fi
 
 # Logrotate
 echo '==> Running logrotate.'
-/usr/sbin/logrotate -s ~/logrotate_status /home/minetest/scripts/logrotate_conf
+/usr/sbin/logrotate -s /home/minetest/logrotate_status /home/minetest/scripts/logrotate_conf
 
 # Update files from the git repo
 echo '==> Running script update.sh.'
